fc51b89be75d14f7e41f2909e07b0f5713932ac4
Jefferson <jefferson@educationjourney.com>
2024-06-05 03:55:34 -0300

    feat: add PesquisaForm

---
+++ app/view/PesquisaForm.php
@@ -0,0 +1,5 @@
+<form action="../app/controller/PesquisaController.php" method="post">
+    <label for="nis">NIS:</label><br>
+    <input type="text" id="nis" name="nis"><br>
+    <input type="submit" value="Pesquisar">
+</form>
