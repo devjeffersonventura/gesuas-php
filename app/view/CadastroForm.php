c9f06cb2922ab854a8b144034f4a7fd083e45907
Jefferson <jefferson@educationjourney.com>
2024-06-05 03:55:24 -0300

    feat: add CadastroForm

---
+++ app/view/CadastroForm.php
@@ -0,0 +1,5 @@
+<form action="../app/controller/CadastroController.php" method="post">
+    <label for="nome">Nome:</label><br>
+    <input type="text" id="nome" name="nome"><br>
+    <input type="submit" value="Cadastrar">
+</form>
